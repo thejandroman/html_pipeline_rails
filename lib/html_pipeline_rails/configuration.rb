32ae0fe8ffeaeba4e3d5a98c36cd6177d8226f0f
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-08-14 15:11:37 -0400

    Add ability to change file extsions in config block

--- lib/html_pipeline_rails/configuration.rb
+++ lib/html_pipeline_rails/configuration.rb
@@ -1,9 +1,11 @@
 module HtmlPipelineRails
   class Configuration
     attr_accessor :pipeline
+    attr_accessor :file_extensions
 
     def initialize
       self.reset!
+      self.file_extensions = :md, :mdown, :markdown
     end
 
     def reset!
